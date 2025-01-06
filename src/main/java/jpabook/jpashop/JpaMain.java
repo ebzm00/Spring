020a17dd6c1fae39b21637c5f0e1aad360c021fa
Teddy <ebzm00@naver.com>
2025-01-06 15:22:09 +0900

    250106 자바 ORM 표준 JPA Commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            //code
+            em.close();
+        }
+        emf.close();
+    }
+}
